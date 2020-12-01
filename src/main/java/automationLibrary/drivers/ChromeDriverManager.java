3f405500ccf92167b58c85ce5ecadf6e474eda58
Nam Trinh Hoai <namth2@sendo.vn>
2020-12-01 23:29:13 +0700

    Update

--- src/main/java/automationLibrary/drivers/ChromeDriverManager.java
+++ src/main/java/automationLibrary/drivers/ChromeDriverManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +15,18 @@ public class ChromeDriverManager extends DriverManager {
     @Override
     public void createDriver(boolean isMobileEmulation) {
         String chromedriverPath = "";
-        if(SystemUtils.IS_OS_MAC_OSX) {
+        
+        if(SystemUtils.IS_OS_LINUX) {
+            File file64 = new File(Configurations.CHROMEDRIVER_LINUX64_FILE_PATH);
+         	File file32 = new File(Configurations.CHROMEDRIVER_LINUX32_FILE_PATH);
+    		file64.setExecutable(true);
+    		file32.setExecutable(true);
+    		if(SystemUtils.OS_ARCH.contains("64")) {
+    			chromedriverPath = Configurations.CHROMEDRIVER_LINUX64_FILE_PATH;
+    		} else {
+    			chromedriverPath = Configurations.CHROMEDRIVER_LINUX32_FILE_PATH;
+    		}
+    	} else if(SystemUtils.IS_OS_MAC_OSX) {
             chromedriverPath = Configurations.CHROMEDRIVER_MACOS_FILE_PATH;
         } else if(SystemUtils.IS_OS_WINDOWS) {
             chromedriverPath = Configurations.CHROMEDRIVER_WINDOWS_FILE_PATH;
