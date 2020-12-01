761adbd37ba05ac7af321f1b0bdd428ad511d6aa
Nam Trinh Hoai <namth2@sendo.vn>
2020-12-01 23:39:16 +0700

    Update

--- src/main/java/automationLibrary/initiations/Configurations.java
+++ src/main/java/automationLibrary/initiations/Configurations.java
@@ -3,6 +3,8 @@ package automationLibrary.initiations;
 public class Configurations {
     public static String CHROMEDRIVER_MACOS_FILE_PATH = System.getProperty("user.dir") + "/externals/chromedriver";
     public static String CHROMEDRIVER_WINDOWS_FILE_PATH = System.getProperty("user.dir") + "/externals/chromedriver.exe";
+    public static String CHROMEDRIVER_LINUX64_FILE_PATH = System.getProperty("user.dir") + "/externals/chromedriver_linux64";
+    public static String CHROMEDRIVER_LINUX32_FILE_PATH = System.getProperty("user.dir") + "/externals/chromedriver_linux32";
     public static String TEST_DATA_FILE_PATH = System.getProperty("user.dir") + "/testData/testData.json";
     public static int ELEMENT_WAIT_TIME = 10000;
     public static int POLLING_TIME = 200;
