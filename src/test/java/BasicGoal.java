1ca1b532eb5617ecf49834b65dd228d5685edbf0
Nam Trinh Hoai <namth2@sendo.vn>
2020-12-01 18:09:07 +0700

    Update

--- src/test/java/BasicGoal.java
+++ src/test/java/BasicGoal.java
@@ -1,19 +1,27 @@
-import automationLibrary.actions.BaseAction;
-import com.zebrunner.agent.core.annotation.TestLabel;
-import goBear.initiations.TestBase;
-import goBear.initiations.TestConfigurations;
-import goBear.objects.TravelInsuranceSearch;
-import goBear.pages.HomePage;
-import goBear.pages.TravelResultPage;
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import com.zebrunner.agent.core.annotation.TestLabel;
+
+import goBear.initiations.TestBase;
+import goBear.pages.HomePage;
+import goBear.pages.TravelResultPage;
+
 public class BasicGoal extends TestBase {
+	Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
     @TestLabel(name = "web", value = {"reporting-service:v1.0", "reporting-service:v1.1"})
     @Test
     public void basicGoal() {
         HomePage homePage = new HomePage(this.driver, this.softAssert);
+        LOGGER.info("Select Insurance tab");
         homePage.selectInsuranceTab();
+        LOGGER.info("Go to travel section");
         homePage.goToTravelSection();
+        LOGGER.info("Go to travel result page");
         homePage.goToTravelResultsPage();
 
         TravelResultPage travelResultPage = new TravelResultPage(this.driver, this.softAssert);
