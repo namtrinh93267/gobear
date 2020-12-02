642cade3d32c62394e95853d8ed5918077be881f
Nam Trinh Hoai <namth2@sendo.vn>
2020-12-02 13:57:13 +0700

    Update

--- src/main/java/automationLibrary/drivers/ChromeDriverManager.java
+++ src/main/java/automationLibrary/drivers/ChromeDriverManager.java
@@ -24,14 +24,14 @@ public class ChromeDriverManager extends DriverManager {
         
         ChromeOptions options = new ChromeOptions();
         //options.setHeadless(true);
-		options.addArguments("--disable-web-security");
-		options.addArguments("--disable-popup-blocking");
-		options.addArguments("--window-size=1600,900");
-		options.addArguments("disable-infobars");
-		options.addArguments("--disable-extensions");
-		options.addArguments("--disable-gpu");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--no-sandbox");
+//		options.addArguments("--disable-web-security");
+//		options.addArguments("--disable-popup-blocking");
+//		options.addArguments("--window-size=1600,900");
+//		options.addArguments("disable-infobars");
+//		options.addArguments("--disable-extensions");
+//		options.addArguments("--disable-gpu");
+//		options.addArguments("--disable-dev-shm-usage");
+//		options.addArguments("--no-sandbox");
 		 
     	Map<String, Object> prefs = new HashMap<String, Object>();
     	prefs.put("credentials_enable_service", false);
@@ -64,7 +64,7 @@ public class ChromeDriverManager extends DriverManager {
             options.setExperimentalOption("mobileEmulation", mobileEmulation);
         }
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        driver = new ChromeDriver(capabilities);
+        driver = new RemoteWebDriver(capabilities);
 //        try {
 //			driver = new RemoteWebDriver(new URL("http://demo:demo@10.90.96.7:80/selenoid/wd/hub"), capabilities);
 //		} catch (MalformedURLException e) {
