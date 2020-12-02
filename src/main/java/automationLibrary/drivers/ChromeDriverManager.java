0b13fe7786652a4211ffea562cc050e346afb5e5
Nam Trinh Hoai <namth2@sendo.vn>
2020-12-02 10:52:49 +0700

    Update

--- src/main/java/automationLibrary/drivers/ChromeDriverManager.java
+++ src/main/java/automationLibrary/drivers/ChromeDriverManager.java
@@ -1,13 +1,15 @@
 package automationLibrary.drivers;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang3.SystemUtils;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import automationLibrary.initiations.Configurations;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -61,6 +63,11 @@ public class ChromeDriverManager extends DriverManager {
             options.setExperimentalOption("mobileEmulation", mobileEmulation);
         }
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-        driver = new ChromeDriver(capabilities);
+        try {
+			driver = new RemoteWebDriver(new URL("http://demo:demo@10.90.96.7:80/selenoid/wd/hub"), capabilities);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
